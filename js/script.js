75a838256e1d28146f4dd3bcb1d84142ec14914b
lmyller <myller42@gmail.com>
2023-01-01 20:27:26 -0300

    Adição funcionalidades principais

---
+++ js/script.js
@@ -0,0 +1,297 @@
+const btn = document.querySelector('#btn');
+const selectCountry = document.querySelector("#choose-country");
+const selectIndicator = document.querySelector("#choose-indicator");
+const start = 2000;
+const end = 2020;
+let resultado = document.querySelector('#resultado');
+let jsonCountries;
+let lista = [];
+
+fetch('https://servicodados.ibge.gov.br/api/v1/paises?lang=EN')
+    .then(response => response.json())
+    .then(json => createCountries(json));
+
+for (let i = 1; i <= 50; i++) {
+    fetch('http://api.worldbank.org/v2/indicator?page=' + i + '&format=json')
+        .then(response => response.json())
+        .then(json => createIndicator(json[1]))
+}
+
+function createCountries(countries) {
+    jsonCountries = countries;
+
+    let lista = [];
+
+    for (let value of countries) {
+        if (value['nome']['abreviado'] !== lista[0]) {
+            lista[0] = value['nome']['abreviado']
+            let option = document.createElement('option');
+            option.text = value['nome']['abreviado'];
+            selectCountry.add(option, null);
+        }
+    }
+}
+
+function createIndicator(indicators) {
+    for (let value of indicators) {
+        let option1 = document.createElement('option');
+        option1.text = value['name'];
+        selectIndicator.add(option1, null);
+        lista.push({ 'nome': value['name'], 'id': value['id'] })
+    }
+}
+
+btn.onclick = function () {
+    removeButtons();
+    removeTable();
+    verifyCountry(jsonCountries, selectCountry.value);
+    createListCountries(jsonCountries);
+}
+
+function removeButtons(){
+    const listCountries = document.querySelector('.list-group');
+
+    if(listCountries.hasChildNodes()){
+        while (listCountries.firstChild)
+            listCountries.removeChild(listCountries.lastChild);
+    }
+}
+
+function searchIndicatorCountry(btnCountry) {
+    removeButtons();
+    removeTable();
+    verifyCountry(jsonCountries, btnCountry);
+    createListCountries(jsonCountries);
+}
+
+function createListCountries(jsonCountries) {
+    const listCountries = document.querySelector('.list-group');
+
+    for (let i = 0; i < 18; i++) {
+        const country = jsonCountries[getRandomNumber(0, jsonCountries.length)]['nome']['abreviado'];
+        button = document.createElement('button');
+        button.setAttribute('type', 'button');
+        button.setAttribute('data-index', 'i');
+        button.setAttribute('class', 'list-group-item list-group-item-action');
+        button.setAttribute('onclick', 'searchIndicatorCountry("' + country + '")');
+        button.innerHTML = country;
+        listCountries.appendChild(button);
+    }
+}
+
+function verifyCountry(countries, country) {
+    for (let value of countries)
+        if (value['nome']['abreviado'] === country)
+            dataCountry(value['id']['ISO-3166-1-ALPHA-2'], country);
+}
+
+function dataCountry(code, country) {
+    fetch('https://servicodados.ibge.gov.br/api/v1/paises/' + code)
+        .then(response => response.json())
+        .then(json => verifyIndicators(json[0]['unidades-monetarias'][0]['id']['ISO-4217-ALPHA'], code, country));
+}
+
+function verifyIndicators(money, code, country) {
+    let id;
+
+    if (money === 'VES')
+        money = 'VEF';
+
+    for (let value of lista) {
+        if (value['nome'] === selectIndicator.value)
+            id = { 'id': value['id'], 'name': selectIndicator.value };
+    }
+
+    fetch('http://api.worldbank.org/v2/country/' + code.toLowerCase() + '/indicator/' + id['id'] + '?date=' + start + ':' + end + '&format=json')
+        .then(response => response.json())
+        .then(json => verifyMoney(json[1], money, id['name'], country));
+}
+
+function verifyMoney(json, money, indicator, country) {
+    let date = [];
+    let data = [];
+    const noGraph = document.querySelector('#graph');
+    const chart = document.querySelector('#myChart');
+
+    if (json !== null)
+        for (let value of json)
+            if (value['value'] !== null) {
+                date.push(value['date']);
+                data.push(value['value']);
+            }
+
+    date = date.sort();
+    data = data.sort();
+
+    if (data.length === 0 || date.length === 0) {
+        chart.style.display = 'none';
+        noGraph.style.display = 'block';
+        noGraph.innerHTML = 'NO DATA AVAILABLE TO ' + indicator.toUpperCase() + ' TO ' + country.toUpperCase();
+        return;
+    }
+
+    if (noGraph.style.display === 'block') {
+        noGraph.style.display = 'none';
+        chart.style.display = 'block';
+    }
+
+    fetch('http://127.0.0.1:2000/api/v1/money/' + money + '/' + 'start_time' + date[0] + ':end_time' + date[date.length - 1])
+        .then(response => response.json())
+        .then(json => createGraph(json, date, data, money, indicator));
+}
+
+function createGraph(json, date, data, money, indicator) {
+    const myChart = document.querySelector('#myChart');
+    let data_money = []
+
+    myChart.style.display = 'block';
+
+
+    if (json.length !== 0) {
+        data_money = average_money(json)
+
+        new Chart('myChart', {
+            type: 'line',
+            data: {
+                labels: date,
+                datasets: [{
+                    data: data,
+                    label: indicator,
+                    borderColor: 'red',
+                    pointBackgroundColor: 'grey',
+                    pointBorderColor: 'grey',
+                    fill: false,
+                }, {
+                    data: data_money,
+                    label: 'Average Price: USD - ' + money,
+                    borderColor: "green",
+                    pointBackgroundColor: 'grey',
+                    pointBorderColor: 'grey',
+                    fill: false
+                }]
+            },
+            options: {
+                legend: {
+                    display: true,
+                    position: 'bottom'
+                },
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            color: 'grey'
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: 'grey'
+                        }
+                    }]
+                }
+            }
+        });
+
+        createTable(data, date, data_money, money, indicator);
+    }
+
+    else {
+        new Chart('myChart', {
+            type: 'line',
+            data: {
+                labels: date,
+                datasets: [{
+                    data: data,
+                    label: indicator,
+                    borderColor: "red",
+                    fill: false,
+                }]
+            },
+            options: {
+                legend: {
+                    display: true,
+                    position: 'bottom'
+                },
+            }
+        });
+    }
+}
+
+function createTable(data, date, data_money, money, indicator) {
+    const tableChart = document.querySelector('#table-chart');
+    
+    removeTable();
+
+    if (data.length !== 0 || date.length !== 0) {
+        let thead = document.createElement('thead');
+        let tbody = document.createElement('tbody');
+        let row_1 = document.createElement('tr');
+        let heading_date = document.createElement('th');
+        heading_date.innerHTML = 'Date';
+        heading_date.setAttribute('scope', 'col');
+        let heading_data = document.createElement('th');
+        heading_data.innerHTML = indicator;
+        heading_data.setAttribute('scope', 'col');
+
+        tableChart.appendChild(thead);
+        tableChart.appendChild(tbody);
+
+        row_1.appendChild(heading_date);
+        row_1.appendChild(heading_data);
+
+        if (data_money.length !== 0) {
+            let heading_money = document.createElement('th');
+            heading_money.innerHTML = 'USD - ' + money;
+            heading_money.setAttribute('scope', 'col');
+            row_1.appendChild(heading_money);
+        }
+
+        thead.appendChild(row_1);
+
+        for (let i = date.length - 1; i >= 0; i--) {
+            let row = document.createElement('tr');
+            let heading_date = document.createElement('td');
+            heading_date.innerHTML = date[i];
+            heading_date.setAttribute('scope', 'row');
+            let heading_data = document.createElement('td');
+            heading_data.innerHTML = data[i];
+            heading_data.setAttribute('scope', 'row');
+
+            row.appendChild(heading_date);
+            row.appendChild(heading_data);
+
+            if (data_money.length !== 0) {
+                let heading_money = document.createElement('td');
+                heading_money.innerHTML = data_money[i];
+                heading_money.setAttribute('scope', 'row');
+                row.appendChild(heading_money);
+            }
+
+            tbody.appendChild(row);
+        }
+
+        tableChart.style.display = 'block';
+    }
+}
+
+function removeTable(){
+    const tableChart = document.querySelector('#table-chart');
+    
+    if(tableChart.hasChildNodes()){
+        while(tableChart.firstChild)
+            tableChart.removeChild(tableChart.lastChild);
+    }
+}
+
+function average_money(json) {
+    data_money = []
+
+    for (let value of json)
+        data_money.push(value['average-price'])
+
+    return data_money.sort();
+}
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
---
+++ py/apimoney.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+from flask import Flask
+from flask import request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+def json_money(data):
+    datas = []
+    data_money = []
+
+    data_json = []
+
+    for value in data:
+        datas.append(value.find_all('td'))
+
+    for row in datas:
+        data_year = []
+        for column in row:
+            column = str(column)
+            column = column.replace('<td>', '').replace('</td>', '')
+            data_year.append(column)
+
+        data_money.append(data_year)
+
+    for value in data_money:
+        data_json.append({"year": value[0], "average-price": value[1]})
+
+    return json.dumps(data_json)
+
+
+@app.route('/api/v1/money/<string:money>/start_time<int:start_time>:end_time<int:end_time>', methods=['GET'])
+def return_data_money(money, start_time, end_time):
+    r = requests.get(
+        'https://fxtop.com/pt/historico-das-taxas-de-cambio.php?YA=1&C1=USD&C2={}&A=1&YYYY1={}&MM1=01&DD1=01&YYYY2={}&MM2=12&DD2=30&LANG=en'.format(money, start_time, end_time))
+
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    table = soup.find("table", border="1")
+
+    if table is None:
+        return []
+
+    table = table.find_all('tr')
+
+    data = [d for d in table]
+
+    data.pop(0)
+
+    return json_money(data)
+
+
+if __name__ == "__main__":
+    app.run(port=2000, debug=True)
